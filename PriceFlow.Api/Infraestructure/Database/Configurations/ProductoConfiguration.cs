b8ef45db216a3e246dbaa767c4af1492935b5fd8
Milagros-Trejo <82959873+Milagros-Trejo@users.noreply.github.com>
2025-07-24 18:02:39 -0300

    Add configurations

---
+++ PriceFlow.Api/Infraestructure/Database/Configurations/ProductoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PriceFlow.Api.Domain.Entities;
+
+namespace PriceFlow.Api.Infraestructure.Database.Configurations
+{
+    public class ProductoConfiguration : IEntityTypeConfiguration<Producto>
+    {
+        public void Configure(EntityTypeBuilder<Producto> builder)
+        {
+            builder.ToTable("productos");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Activo).IsRequired();
+            builder.Property(p => p.Sku).IsRequired().HasColumnType("varchar(50)");
+            builder.Property(p => p.Marca).IsRequired().HasColumnType("varchar(100)");
+
+            builder.HasIndex(p => p.Sku).IsUnique(true);
+        }
+    }
+}
